e3f9704973b58d4294b987f80f50e8f02c303975
Arnaud Ligny <arnaud@ligny.org>
2019-06-17 12:11:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 USER_NAME="$GITHUB_ACTOR"
@@ -8,11 +7,13 @@ REPOSITORY="$GITHUB_REPOSITORY"
 REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)"
 OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)"
 GHIO="${OWNER}.github.io"
+# target branch
 if [[ "$REPONAME" == "$GHIO" ]]; then
   TARGET_BRANCH="master"
 else
   TARGET_BRANCH="gh-pages"
 fi
+# build dir
 if [ -z "$SITE_DIR" ]
 then
     SITE_DIR="_site"
@@ -30,16 +31,18 @@ rm -rf gh-pages/*
 cp -R $HOME/.git gh-pages/.git
 cd gh-pages
 cp -Rf $HOME/$SITE_DIR/* .
+# custom domain?
 if [ ! -z "$CNAME" ]
 then
     echo "$CNAME" > CNAME
 fi
-if [ ! -z "$NO_JEKYLL" ]
+# .nojekyll
+if [ "$JEKYLL_SITE" == "NO" ]
 then
-    echo "It is not a Jekyll site!" > .nojekyll
+    touch .nojekyll
 fi
 git add -Af .
-git commit -m "$USER_NAME push updated website"
+git commit -m "$USER_NAME published a site update"
 git push -fq origin $TARGET_BRANCH > /dev/null
 
 echo "### Finished deploy"
