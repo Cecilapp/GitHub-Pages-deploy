34013b508e103a7d3af5e5b405aeef766b9b1f84
Arnaud Ligny <arnaud@ligny.org>
2019-06-17 15:28:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -e
 
-USER_NAME="$GITHUB_ACTOR"
 # commiter email
 if [ -z "$EMAIL" ]
 then
   echo "A verified email is required"
   exit 1
 fi
-USER_EMAIL="$EMAIL"
-REPOSITORY="$GITHUB_REPOSITORY"
 REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)"
 OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)"
 GHIO="${OWNER}.github.io"
@@ -20,23 +17,23 @@ else
   TARGET_BRANCH="gh-pages"
 fi
 # build dir
-if [ -z "$SITE_DIR" ]
+if [ -z "$BUILD_DIR" ]
 then
-  SITE_DIR="_site"
+  BUILD_DIR="_site"
 fi
 
-echo "### Started deploy to $REPOSITORY/$TARGET_BRANCH"
+echo "### Started deploy to $GITHUB_REPOSITORY/$TARGET_BRANCH"
 
-cp -R $SITE_DIR $HOME/$SITE_DIR
+cp -R $BUILD_DIR $HOME/$BUILD_DIR
 cd $HOME
-git config --global user.name "$USER_NAME"
-git config --global user.email "$USER_EMAIL"
-git clone --quiet --branch=$TARGET_BRANCH https://${GITHUB_TOKEN}@github.com/${REPOSITORY}.git $TARGET_BRANCH > /dev/null
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "$EMAIL"
+git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git $TARGET_BRANCH > /dev/null
 cp -R gh-pages/.git $HOME/.git
 rm -rf gh-pages/*
 cp -R $HOME/.git gh-pages/.git
 cd gh-pages
-cp -Rf $HOME/$SITE_DIR/* .
+cp -Rf $HOME/${BUILD_DIR}/* .
 # custom domain?
 if [ ! -z "$CNAME" ]
 then
@@ -50,7 +47,7 @@ then
   touch .nojekyll
 fi
 git add -Af .
-git commit -m "$USER_NAME published a site update"
+git commit -m "$GITHUB_ACTOR published a site update"
 git push -fq origin $TARGET_BRANCH > /dev/null
 
 echo "### Finished deploy"
